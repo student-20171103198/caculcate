5176b982b1f2b887e8e180943af2be4c7c88f199
student-20171103198 <744136379@qq.com>
2018-10-27 08:56:27 +0800

    Initial Commit

---
+++ caculcate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  caculcate
+//
+//  Created by s20171103198 on 2018/10/27.
+//  Copyright © 2018 s20171103198. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ caculcateTests/caculcateTests.swift
@@ -0,0 +1,34 @@
+//
+//  caculcateTests.swift
+//  caculcateTests
+//
+//  Created by s20171103198 on 2018/10/27.
+//  Copyright © 2018 s20171103198. All rights reserved.
+//
+
+import XCTest
+@testable import caculcate
+
+class caculcateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
